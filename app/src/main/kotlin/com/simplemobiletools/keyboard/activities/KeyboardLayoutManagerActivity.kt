a088f95282051ebc1cf29c54d23b987d5e35eada
codeBodger <rnackerman762+github@gmail.com>
2022-12-14 11:12:27 -0500

    I don't know, but something broke

--- app/src/main/kotlin/com/simplemobiletools/keyboard/activities/KeyboardLayoutManagerActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/keyboard/activities/KeyboardLayoutManagerActivity.kt
@@ -1,11 +1,33 @@
-package com.simplemobiletools.keyboard.activities
-
-import android.view.View
-import android.widget.RelativeLayout
-import androidx.recyclerview.widget.RecyclerView
-
-class KeyboardLayoutManagerActivity {
-    class LayoutManagerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val relLay: RelativeLayout = view.
-    }
-}
+//package com.simplemobiletools.keyboard.activities
+//
+//import android.content.Context
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.recyclerview.widget.RecyclerView
+//import com.simplemobiletools.keyboard.R
+//import com.simplemobiletools.keyboard.databinding.KeyboardLayoutListBinding
+//
+//
+//class KeyboardLayoutManagerActivity : AppCompatActivity() {
+//
+//    class ListAdapter : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+//
+//        val mContext: Context
+//
+//
+//        fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//            val bi: ListItemBinding = DataBindingUtil.inflate(LayoutInflater.from(mContext), R.layout.list_item, parent, false)
+//            return ViewHolder(bi)
+//        }
+//
+//        class ViewHolder(itemView: KeyboardLayoutListBinding) : RecyclerView.ViewHolder(itemView.getRoot()) {
+//            var bi: ListItemBinding
+//
+//            init {
+//                bi = itemView
+//            }
+//        }
+//    }
+//
+//}
